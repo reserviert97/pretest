50cb5b60a5e1b9d30a85a97977be6454ce2b7d45
reserviert97 <nurlatif97@outlook.com>
2019-06-15 20:02:44 +0700

    nomor 3

--- 2.js
+++ 2.js
@@ -1,23 +1,26 @@
-const userInput = 5
+const userInput = 5;
+
+const p = [];
+for (let i = 1; i <= 100; i++) {
+  let a = 0;
+  for (let j = 1; j <= i; j++) {
+    if (i % j == 0) {
+      a++;
+    }
+  }
+  if (a == 2) {
+    p.push(i);
+  }
+}
 
 if (userInput > 0 && userInput < 10) {
   for (let baris = 1; baris <= 5; baris++) {
-    let print = ''
+    let print = "";
     for (let kolom = 0; kolom < baris; kolom++) {
-      var p = []
-      for(let i = 1; i <= 14; i++){
-        let a = 0
-        for (let j = 1; j <= i; j++) {
-          if (i % j == 0) {
-            a++
-          }
-        }
-        if (a == 2) {
-          p.push(i)
-        }
-      }
-      print += p[kolom] + ' '
+      print += p[kolom] + " ";
     }
     console.log(print);
-}
+  }
+}else {
+  console.log('Input harus lebih besar dari 0 dan lebih kecil dari 10');
 }
---
+++ 3.js
@@ -0,0 +1,20 @@
+let musim = 'semi'
+let tinggiPohon = 1
+let totalTahun = 4
+
+function pohonImajinasi(x, y) {
+    if (y == 0) {
+        return 'Tinggi akhir pohon ' + x
+    }
+    // Musim Semi
+    x = x * 2
+    console.log('Musim semi berakhir tinggi pohon saat ini : '+x)
+
+    // Musim Gugur
+    x = x + 1
+    console.log('Musim gugur berakhir tinggi pohon saat ini : '+x)
+
+    return pohonImajinasi(x, y-1)
+}
+
+console.log(pohonImajinasi(2, 5))
