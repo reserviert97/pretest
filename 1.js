b3a5dcbdb2425da195611e71e437d29d12206f84
reserviert97 <nurlatif97@outlook.com>
2019-06-15 17:38:13 +0700

    soal no 2

--- 1.js
+++ 1.js
@@ -1,8 +1,8 @@
 let uang = 50000
 let hargaMie = 2500
 
-let totalBeli = uang / hargaMie
-let totalBonus = totalBeli / 4
+let totalBeli = Math.floor(uang / hargaMie)
+let totalBonus = Math.floor(totalBeli / 4)
 let total = totalBeli + totalBonus
 
 console.log(`Total mie yang didapatkan si anak : ${total}`)
--- 2.js
+++ 2.js
@@ -1,17 +1,23 @@
-const alas = 5
-const tinggi = 5
+const userInput = 5
 
-if ( 0 < alas < 10 ) {
-  for (let i = 1; i <= 5; i++) {
-    var string = ""
-    for (let j = 1; j <= i; j++) {
-      string += "*"
+if (userInput > 0 && userInput < 10) {
+  for (let baris = 1; baris <= 5; baris++) {
+    let print = ''
+    for (let kolom = 0; kolom < baris; kolom++) {
+      var p = []
+      for(let i = 1; i <= 14; i++){
+        let a = 0
+        for (let j = 1; j <= i; j++) {
+          if (i % j == 0) {
+            a++
+          }
+        }
+        if (a == 2) {
+          p.push(i)
+        }
+      }
+      print += p[kolom] + ' '
     }
-    console.log(string);
-  }
-}else {
-  console.log("maaf angka alas harus lebih besar dari nol dan lebih kecil dari 10");
-  
+    console.log(print);
 }
-
-
+}
